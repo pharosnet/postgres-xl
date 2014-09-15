a1b5ed17fbf7061c6b2ed717999615668ef8d28c
Pavan Deolasee <pavan.deolasee@gmail.com>
2014-09-15 16:58:55 +0530

    Fix a bug which was causing datanode slave to start in ASYNC replication
    We document and recommend setting up datanode slaves in SYNC replication to
    ensure zero data-loss in case of failure. But the pgxc_ctl was doing it wrong.
    This commit fixes that

--- contrib/pgxc_ctl/coord_cmd.c
+++ contrib/pgxc_ctl/coord_cmd.c
@@ -1768,7 +1768,7 @@ cmd_t *prepare_startCoordinatorSlave(char *nodeName)
 			aval(VAR_coordNames)[idx]);
 	fclose(f);
 		
-	/* Reloae postgresql.conf change */
+	/* Reload postgresql.conf change */
 	appendCmdEl(cmdPgCtlStart, (cmdMasterReload = initCmd(aval(VAR_coordMasterServers)[idx])));
 	snprintf(newCommand(cmdMasterReload), MAXLINE,
 			 "pg_ctl reload -Z coordinator -D %s",
