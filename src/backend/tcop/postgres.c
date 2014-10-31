3e4346857d73841373b3424147e104a58802f8f3
Pavan Deolasee <pavan.deolasee@gmail.com>
2014-10-31 13:11:38 +0530

    Allow pg_stat_statements to work with XL
    XL uses a post-post-analyze hook for it's internal purposes. But this overrides
    any existing hooks, including that of pg_stat_statements. Setup things so that
    the previous hook gets called appropriately

--- src/backend/tcop/postgres.c
+++ src/backend/tcop/postgres.c
@@ -4285,6 +4285,8 @@ PostgresMain(int argc, char *argv[], const char *username)
 	}
 
 	/* Set up the post parse analyze hook */
+	if (post_parse_analyze_hook)
+		prev_ParseAnalyze_callback = post_parse_analyze_hook;
 	post_parse_analyze_hook = ParseAnalyze_callback;
 
 	/* if we exit, try to release cluster lock properly */
