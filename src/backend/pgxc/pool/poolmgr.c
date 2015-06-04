633da80d8080348ae59dcdd1404a061abc8d0ead
Pavan Deolasee <pavan.deolasee@gmail.com>
2015-06-04 15:52:39 +0530

    Do not call set_sid() for pooler processes.
    A temp fix so that other tests can progress.

--- src/backend/pgxc/pool/poolmgr.c
+++ src/backend/pgxc/pool/poolmgr.c
@@ -246,16 +246,6 @@ PoolManagerInit()
 	ForgetLockFiles();	
 
 	/*
-	 * If possible, make this process a group leader, so that the postmaster
-	 * can signal any child processes too.	(pool manager probably never has any
-	 * child processes, but for consistency we make all postmaster child
-	 * processes do this.)
-	 */
-#ifdef HAVE_SETSID
-	if (setsid() < 0)
-		elog(FATAL, "setsid() failed: %m");
-#endif
-	/*
 	 * Properly accept or ignore signals the postmaster might send us
 	 */
 	pqsignal(SIGINT, pooler_die);
