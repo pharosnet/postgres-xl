893bdf3baa52cd8135d8939c3f86d6d5e105e793
Pallavi Sontakke <pallavi.sontakke@2ndQuadrant.com>
2016-02-12 18:20:37 +0530

    Test sql and output changes for LATERAL issue
    Separate out Issue #73 in Issue Tracker.
    Accept XL query plans.

--- contrib/tsm_system_time/sql/tsm_system_time.sql
+++ contrib/tsm_system_time/sql/tsm_system_time.sql
@@ -38,14 +38,13 @@ SELECT * FROM
   LATERAL (SELECT COUNT(*) FROM test_tablesample
            TABLESAMPLE system_time (time)) ss;
 
-SELECT * FROM
-  (VALUES (0),(100000)) v(time),
-  LATERAL (SELECT COUNT(*) FROM test_tablesample
-           TABLESAMPLE system_time (time)) ss;
-
 CREATE VIEW vv AS
   SELECT * FROM test_tablesample TABLESAMPLE system_time (20);
 
 EXPLAIN (COSTS OFF) SELECT * FROM vv;
 
 DROP EXTENSION tsm_system_time;  -- fail, view depends on extension
+
+DROP VIEW vv;
+DROP TABLE test_tablesample;
+DROP EXTENSION tsm_system_time;
