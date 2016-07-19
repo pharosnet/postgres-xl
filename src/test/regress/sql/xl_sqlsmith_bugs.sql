1883c3a5fb7dcb3ebecb0aba491a766e0573e0ac
Pallavi Sontakke <pallavi.sontakke@2ndQuadrant.com>
2016-07-19 15:24:19 +0530

    Add test case for Issue #88

--- src/test/regress/sql/xl_sqlsmith_bugs.sql
+++ src/test/regress/sql/xl_sqlsmith_bugs.sql
@@ -1,3 +1,28 @@
+-- #88
+-- SQLsmith: Error: unsupported data type for HASH locator:
+
+CREATE TABLE xl_join_t1 (val1 int, val2 int);
+CREATE TABLE xl_join_t2 (val1 int, val2 int);
+CREATE TABLE xl_join_t3 (val1 int, val2 int);
+INSERT INTO xl_join_t1 VALUES (1,10),(2,20);
+INSERT INTO xl_join_t2 VALUES (3,30),(4,40);
+INSERT INTO xl_join_t3 VALUES (5,50),(6,60);EXPLAIN SELECT * FROM xl_join_t1
+        INNER JOIN xl_join_t2 ON xl_join_t1.val1 = xl_join_t2.val2
+        INNER JOIN xl_join_t3 ON xl_join_t1.val1 = xl_join_t3.val1;
+SELECT * FROM xl_join_t1
+        INNER JOIN xl_join_t2 ON xl_join_t1.val1 = xl_join_t2.val2
+        INNER JOIN xl_join_t3 ON xl_join_t1.val1 = xl_join_t3.val1;
+
+
+insert into public.xl_join_t1 values (
+        (select character_maximum_length from information_schema.routines limit 1 offset 15)
+        ,
+        33);
+
+DROP TABLE xl_join_t1;
+DROP TABLE xl_join_t2;
+DROP TABLE xl_join_t3;
+
 -- #87
 -- SQLsmith error: cache lookup failed for function 0
 
