30e0ffac619348096648b150f2bd18d0f8e2cf3b
Tomas Vondra <tomas@2ndquadrant.com>
2016-08-24 12:05:24 +0200

    remove pghba contrib module
    The purpose of the module is unknown, and it does not even compile
    for quite some time, so apparently no one uses it. Instead of fixing
    it, let's remove it - if someone realizes it's useful, we can get
    it from history.

--- contrib/pghba/pghba--unpackaged--1.0.sql
+++
@@ -1,6 +0,0 @@
-/* contrib/pghba/pghba--unpackaged--1.0.sql */
-
--- complain if script is sourced in psql, rather than via CREATE EXTENSION
-\echo Use "CREATE EXTENSION pghba" to load this file. \quit
-
-ALTER EXTENSION pghba ADD view pghba;
